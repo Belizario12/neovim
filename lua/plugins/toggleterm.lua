cdc0e6d149a88d72a07db86e08c32cb7c91f982d
LUCAS NEVES BELIZARIO <106932897+Belizario12@users.noreply.github.com>
2024-02-26 14:20:58 -0300

    Feat: Adicionando lógica para multi-terminais.

--- lua/plugins/toggleterm.lua
+++ lua/plugins/toggleterm.lua
@@ -5,19 +5,14 @@ return {
 			vim.keymap.set("n", "TT", ":ToggleTerm direction=horizontal name=desktop<CR>", {}),
 			vim.keymap.set("t", "<esc>", [[<C-\><C-n>]], {}),
 			vim.keymap.set("n", "TS", ":TermSelect<CR>", {}),
+      vim.keymap.set("n", "<C-t>", function()
+                local new_term = require("toggleterm.terminal").Terminal:new({
+                    direction = "horizontal", -- Ou "horizontal" se preferir
+                })
+                new_term:toggle()
+            end, { noremap = true, silent = true }),
 
-			vim.keymap.set("n", "<C-T>", function()
-				local current_term = require("toggleterm.terminal").get_current_terminal()
-				if current_term then
-					current_term:toggle()
-					local new_term = require("toggleterm.terminal").Terminal:new({
-						direction = "vertical", -- Ou "horizontal" se preferir
-					})
-					new_term:toggle()
-				else
-					print("Nenhum terminal aberto atualmente")
-				end
-			end),
-		})
-	end,
+            -- Outros mapeamentos e configurações aqui...
+        })
+    end,
 }
