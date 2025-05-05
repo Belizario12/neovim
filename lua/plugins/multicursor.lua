da1601ca72cae25e048e8525633810f16f150dd9
Belizario <lucasnbelizario@gmail.com>
2025-05-05 08:58:37 -0300

    Feat: Atualizando plugin de multicursor

--- lua/plugins/multicursor.lua
+++ lua/plugins/multicursor.lua
@@ -1,17 +1,15 @@
 return {
-	"smoka7/multicursors.nvim",
-	event = "VeryLazy",
-	dependencies = {
-		"smoka7/hydra.nvim",
-	},
-	opts = {},
-	cmd = { "MCstart", "MCvisual", "MCclear", "MCpattern", "MCvisualPattern", "MCunderCursor" },
-	keys = {
-		{
-			mode = { "v", "n" },
-			"<C-d>",
-			"<cmd>MCstart<cr>",
-			desc = "Create a selection for selected text or word under the cursor",
-		},
-	},
+  "mg979/vim-visual-multi",
+  branch = "master",
+  event = "VeryLazy",
+  init = function()
+    vim.g.VM_default_mappings = 1 -- usa os atalhos padrão
+    vim.g.VM_maps = {
+      ["Find Under"]         = "<C-d>", -- mesmo atalho que você usava
+      ["Find Subword Under"] = "<C-d>",
+      ["Select All"]         = "\\A",   -- você pode customizar
+      ["Add Cursor Up"]      = "<C-Up>",
+      ["Add Cursor Down"]    = "<C-Down>",
+    }
+  end,
 }
